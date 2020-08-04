f932f34f1f257b853d6a9924f00af4e9e181e65e
REAL MADRID for ever <matrix.xxcs102@hotmail.com>
2020-08-04 10:26:34 +0300

    price

---
+++ database/factories/RestaurantVedioFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\RestaurantVedio;
+use Faker\Generator as Faker;
+
+$factory->define(RestaurantVedio::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
